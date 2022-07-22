f8ae5dad6c1113d6862e0987c26fd62787e00261
Vassori <o.komissarov.job@gmail.com>
2022-07-22 12:40:20 +0300

    0. Что такое тестирование [#159431]

--- src/main/java/ru/job4j/calculator/Fit.java
+++ src/main/java/ru/job4j/calculator/Fit.java
@@ -10,10 +10,10 @@ public class Fit {
     }
 
     public static void main(String[] args) {
-        short height = 187;
+        short height = 180;
         double man = Fit.manWeight(height);
-        System.out.println("Man 187 is " + man);
+        System.out.println("Man 180 is " + man);
         double woman = Fit.womanWeight(height);
-        System.out.println("Woman 187 is " + woman);
+        System.out.println("Woman 180 is " + woman);
     }
 }
