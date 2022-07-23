8c78048670d61f16a8f86d8163f34407df3ed807
Vassori <o.komissarov.job@gmail.com>
2022-07-23 14:16:26 +0300

    3. Тесты для определения расстояния между точками. [#159459]

--- src/main/java/ru/job4j/condition/Point.java
+++ src/main/java/ru/job4j/condition/Point.java
@@ -6,9 +6,9 @@ public class Point {
     }
 
     public static void main(String[] args) {
-        double result = Point.distance(0, 0, 2, 0);
-        double result2 = Point.distance(1, 2, 3, 4);
-        System.out.println("result (0, 0) to (2, 0): " + result);
-        System.out.println("result (1, 2) to (3, 4): " + result2);
+        double outResult = Point.distance(0, 0, 2, 0);
+        double outResult2 = Point.distance(1, 2, 3, 4);
+        System.out.println("result (0, 0) to (2, 0): " + outResult);
+        System.out.println("result (1, 2) to (3, 4): " + outResult2);
     }
 }
