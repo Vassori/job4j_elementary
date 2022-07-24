727b3a06eda8aa9f46ab4bd6fd27a6e1ee588f53
Vassori <o.komissarov.job@gmail.com>
2022-07-24 22:06:36 +0300

    4.1.2. Глупый бот [#31323]

---
+++ src/test/java/ru/job4j/condition/DummyBotTest.java
@@ -0,0 +1,31 @@
+package ru.job4j.condition;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+class DummyBotTest {
+
+    @Test
+    public void whenGreetBot() {
+        String in = "Привет, Бот.";
+        String result = DummyBot.answer(in);
+        String expected = "Привет, умник.";
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenByeBot() {
+        String in = "Пока.";
+        String result = DummyBot.answer(in);
+        String expected = "До скорой встречи.";
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenUnknownBot() {
+        String in = "Сколько будет 2 + 2?";
+        String result = DummyBot.answer(in);
+        String expected = "Это ставит меня в тупик. Задайте другой вопрос.";
+        assertThat(result).isEqualTo(expected);
+    }
+}
