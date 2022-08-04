7dce24b0de9ebf49aa7c308c5f2041f81c3a317a
Vassori <o.komissarov.job@gmail.com>
2022-08-04 20:29:45 +0300

    6.0.3. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,15 @@ public class ArrayDefinition {
         System.out.println("Размер массива равен: " + surnames.length);
         float[] prices = new float[40];
         System.out.println("Размер массива равен: " + prices.length);
+        System.out.println();
+        String[] names = new String[4];
+        names[0] = "Oleg Komissarov";
+        names[1] = "Nikolas Checkulaev";
+        names[2] = "Evgeny Menzhulin";
+        names[3] = "Gleb Krel";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
