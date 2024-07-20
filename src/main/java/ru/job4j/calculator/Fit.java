6a85a1d45a9a90507511e5cf48a78bbeba93149e
Vassori <o.komissarov.job@gmail.com>
2024-07-20 05:58:40 +0300

    GPT. Нейросети спешат на помощь. [#505156]

--- src/main/java/ru/job4j/calculator/Fit.java
+++ src/main/java/ru/job4j/calculator/Fit.java
@@ -1,19 +1,25 @@
 package ru.job4j.calculator;
 
+import java.util.Scanner;
+
 public class Fit {
-    public static double manWeight(short height) {
+    public static double manWeight(int height) {
        return (height - 100) * 1.15;
     }
 
-    public static double womanWeight(short height) {
+    public static double womanWeight(int height) {
         return (height - 110) * 1.15;
     }
 
     public static void main(String[] args) {
-        short height = 180;
-        double outMan = Fit.manWeight(height);
-        System.out.println("Man 180 is " + outMan);
-        double outWoman = Fit.womanWeight(height);
-        System.out.println("Woman 180 is " + outWoman);
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите рост в сантиметрах ");
+        int height = in.nextInt();
+        in.close();
+
+        double resultMan = Fit.manWeight(height);
+        System.out.printf("Man %d cm is %.2f kg%n", height, resultMan);
+        double resultWoman = Fit.womanWeight(height);
+        System.out.printf("Woman %d cm is %.2f kg%n", height, resultWoman);
     }
 }
--- src/test/java/ru/job4j/calculator/FitTest.java
+++ src/test/java/ru/job4j/calculator/FitTest.java
@@ -9,15 +9,15 @@ public class FitTest {
     public void whenManHeight170Then80Dot5() {
         short height = 170;
         double expected = 80.5;
-        double outMan = Fit.manWeight(height);
-        assertThat(outMan).isEqualTo(expected, withPrecision(0.01));
+        double resultMan = Fit.manWeight(height);
+        assertThat(resultMan).isEqualTo(expected, withPrecision(0.01));
     }
 
     @Test
     public void whenWomanWeight170Then63Dot25() {
         short height = 165;
         double expected = 63.25;
-        double outWoman = Fit.womanWeight(height);
-        assertThat(outWoman).isEqualTo(expected, withPrecision(0.01));
+        double resultWoman = Fit.womanWeight(height);
+        assertThat(resultWoman).isEqualTo(expected, withPrecision(0.01));
     }
 }
