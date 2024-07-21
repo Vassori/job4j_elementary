02a3f40f11cbf0b73a51fac7391ba4152dc30b01
Vassori <o.komissarov.job@gmail.com>
2024-07-21 07:16:57 +0300

    GPT. Нейросети спешат на помощь. [#505156 #505929]

--- src/test/java/ru/job4j/calculator/FitTest.java
+++ src/test/java/ru/job4j/calculator/FitTest.java
@@ -9,15 +9,15 @@ public class FitTest {
     public void whenManHeight170Then80Dot5() {
         int height = 170;
         double expected = 80.5;
-        double resultMan = Fit.manWeight(height);
-        assertThat(resultMan).isEqualTo(expected, withPrecision(0.01));
+        double rslMan = Fit.manWeight(height);
+        assertThat(rslMan).isEqualTo(expected, withPrecision(0.01));
     }
 
     @Test
     public void whenWomanWeight170Then63Dot25() {
         int height = 165;
         double expected = 63.25;
-        double resultWoman = Fit.womanWeight(height);
-        assertThat(resultWoman).isEqualTo(expected, withPrecision(0.01));
+        double rslWoman = Fit.womanWeight(height);
+        assertThat(rslWoman).isEqualTo(expected, withPrecision(0.01));
     }
 }
